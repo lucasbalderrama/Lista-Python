2b272a7ba05ab3cdf0a636459fc7e55b411eb67c
KauaAlbuquerqueAlmeida <albuquerquealmeidakaua@gmail.com>
2024-11-26 10:44:30 -0300

    exercicio456

--- Exercício 4/ex4.py
+++ Exercício 4/ex4.py
@@ -1,1 +1,10 @@
 #Gabriel Reis de Brito, Kauã de Albuquerque Almeida e Lucas Randal Abreu Balderrama
+def converter_temperatura(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    kelvin = celsius + 273.15
+    return fahrenheit, kelvin
+
+#exemplo de uso:
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit, kelvin = converter_temperatura(celsius)
+print(f"{celsius}°C equivalem a {fahrenheit:.2f}°F e {kelvin:.2f}K.")
