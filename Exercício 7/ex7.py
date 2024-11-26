c68ecf9c5340010c09405cc7b818d1ba2d66063d
Gabriel-Britoo <gabrito343@gmail.com>
2024-11-26 15:41:23 -0300

    ex7 e ex8

--- Exercício 7/ex7.py
+++ Exercício 7/ex7.py
@@ -1,1 +1,27 @@
 #Gabriel Reis de Brito, Kauâ de Albuquerque Almeida e Lucas Randal Abreu Balderrama
+def cadastra_produto(nome, preco, quant):
+    return{
+        "nome": nome,
+        "preco": preco,
+        "quantidade": quant
+    }
+
+produtos = []
+
+for i in range(5):
+    nome = input(f"\nInsira o nome do produto {i+1}: ")
+    preco = float(input(f"Insira o preço do produto {i+1}: "))
+    quant = int(input(f"Insira a quantidade do produto {i+1}: "))
+
+    produto = cadastra_produto(nome, preco, quant)
+    produtos.append(produto)
+
+    if i < 4:
+        continuar = int(input('\nDigite 1 para continuar adicionando produtos. Digite 0 para encerrar: '))
+
+    if continuar == 0:
+        break
+
+print("\nProdutos:")
+for produto in produtos:
+    print(f"-> Nome: {produto['nome']}; Preço: {produto['preco']}; Quantidade: {produto['quantidade']}")
--- Exercício 8/ex8.py
+++ Exercício 8/ex8.py
@@ -1,1 +1,11 @@
 #Gabriel Reis de Brito, Kauâ de Albuquerque Almeida e Lucas Randal Abreu Balderrama
+import pandas as pd
+
+ler = pd.read_csv(r"C:\Users\Gabriel\Documents\Lista-Python\Exercício 8\temperaturas.csv")
+
+media_janeiro = ler["Janeiro"].mean()
+media_fevereiro = ler["Fevereiro"].mean()
+media_marco = ler["Março"].mean()
+
+print("<- Média de temperaturas ->")
+print(f"Janeiro: {media_janeiro:.1f}°C \nFevereiro: {media_fevereiro:.1f}°C \nMarço: {media_marco:.1f}°C")
