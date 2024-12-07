73b7e3392958df82db72513bca17114ea069c954
Gabriel-Britoo <gabrito343@gmail.com>
2024-12-07 14:11:15 -0300

    fim ex9

--- Exercício 9/ex9.py
+++ Exercício 9/ex9.py
@@ -1,50 +1,38 @@
-#Gabriel Reis de Brito, Kauâ de Albuquerque Almeida e Lucas Randal Abreu Balderrama
+def classificar_impacto(medida, limites):
+    if medida < limites[0]:
+        return "Impacto: Leve"
+    elif limites[0] <= medida <= limites[1]:
+        return "Impacto: Moderado"
+    else:
+        return "Impacto: Alto"
+
+desastres = ["Tremor de terra", "Tsunami", "Deslizamento de terra", 
+             "Ciclone", "Tempestade", "Onda de calor"]
+
+limites = [
+    [3.9, 6.9],
+    [2, 5],
+    [10000, 100000],
+    [153, 208],
+    [50, 100],
+    [38, 42]
+]
+
+print(f"""Medidas: 
+      {desastres[0]}: Escala Ritcher; 
+      {desastres[1]}: Altura da onda (m);
+      {desastres[2]}: Volume de material deslocado (m³);
+      {desastres[3]}: Velocidade dos ventos (km/h);
+      {desastres[4]}: Volume da chuva (mm);
+      {desastres[5]}: Temperatura máxima (°C).\n""")
 
 lista_medidas = []
-desastres= ["Tremor de terra", "Tsunami", "Deslizamento de terra", "Ciclone", "Tempestade", "Onda de calor"]
-
-for i in desastres:
-    medida = int(input(f"Insira a medida do desastre '{i}': "))
+for desastre in desastres:
+    medida = float(input(f"Insira a medida do desastre '{desastre}': "))
     lista_medidas.append(medida)
 
-if lista_medidas[0] < 3.9:
-    print("")
-elif lista_medidas[0] > 6 and lista_medidas[0] < 6.9:
-    print("")
-elif lista_medidas[0] > 7:
-    print("")
+print("<-------------------------------->") # Apenas uma quebra de linha
 
-if lista_medidas[1] < 2:
-    print("")
-elif lista_medidas[1] > 2 and lista_medidas[1] < 5:
-    print("")
-elif lista_medidas[1] > 5:
-    print("")
-
-if lista_medidas[2] < 3.9:
-    print("")
-elif lista_medidas[2] > 6 and lista_medidas[2] < 6.9:
-    print("")
-elif lista_medidas[2] > 7:
-    print("")
-
-if lista_medidas[3] < 3.9:
-    print("")
-elif lista_medidas[3] > 6 and lista_medidas[3] < 6.9:
-    print("")
-elif lista_medidas[3] > 7:
-    print("")
-
-if lista_medidas[4] < 3.9:
-    print("")
-elif lista_medidas[4] > 6 and lista_medidas[4] < 6.9:
-    print("")
-elif lista_medidas[4] > 7:
-    print("")
-
-if lista_medidas[5] < 3.9:
-    print("")
-elif lista_medidas[5] > 6 and lista_medidas[5] < 6.9:
-    print("")
-elif lista_medidas[5] > 7:
-    print("")
+for i, medida in enumerate(lista_medidas):
+    impacto = classificar_impacto(medida, limites[i])
+    print(f"{desastres[i]} - {impacto}")
